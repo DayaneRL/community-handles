0fa5bc2232b17dda04f5db3d491d8d83ae4f3fe8
Dayane Lima <dayanerl.dev@gmail.com>
2024-09-07 13:53:20 -0300

    refatorando para textos em pt-br

--- app/[domain]/create-your-own/page.tsx
+++ app/[domain]/create-your-own/page.tsx
@@ -11,7 +11,7 @@ import {
 import { Stage } from "@/components/stage"
 
 export const metadata = {
-  title: "Create a Community Handle for your community",
+  title: "Crie um handle para sua comunidade",
   description: "Host your own tool",
 }
 
@@ -20,19 +20,18 @@ export default function CommunityPage() {
     <main className="container grid items-center gap-6 pb-8 pt-6 md:py-10">
       <div className="flex max-w-[980px] flex-col items-start gap-4">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter sm:text-3xl md:text-5xl lg:text-6xl">
-          Create a Community Handle <br className="hidden sm:inline" />
-          for your community
+          Crie um Handle da Comunidade <br className="hidden sm:inline" />
+          para a sua comunidade
         </h1>
         <p className="max-w-[700px] text-lg text-muted-foreground sm:text-xl">
-          Want a custom community handle for your community, like
-          @chaewon.fearnot.social? Follow
-          these steps to get one.
+         Quer um handle personalizado para sua comunidade, como
+          @chaewon.fearnot.social? Siga estes passos para obter o seu.
         </p>
       </div>
       <div>
-        <Stage title="Buy a domain" number={1}>
+        <Stage title="Compre um domínio" number={1}>
           <p className="max-w-lg">
-            Buy a domain from a domain registrar. We use{" "}
+          Compre um domínio de um registrador de domínios. Nós usamos{" "}
             <a
               href="https://namecheap.com"
               className="underline"
@@ -41,33 +40,23 @@ export default function CommunityPage() {
             >
               Namecheap
             </a>
-            , but it doesn&apos;t matter which one you use. Just make sure you
-            are able to change where you point the nameservers.
+            , mas não importa qual você usa. 
+            Apenas certifique-se de que você possa alterar o apontamento dos nameservers.
           </p>
         </Stage>
-        <Stage title="Host the Community Handles tool" number={2} last>
-          <p className="max-w-lg">You then need to host the tool.</p>
+        <Stage title="Hospede o serviço Community Handles" number={2} last>
+          <p className="max-w-lg">Em seguida, você precisa hospedar o serviço.</p>
           <p className="mt-4 max-w-lg">
-            If you want to host it yourself,{" "}
+            Se você quiser hospedar por conta própria,{" "}
             <a
               href="https://github.com/mozzius/community-handles"
               className="underline"
             >
-              fork the project on GitHub
+              faça um fork do projeto no GitHub
             </a>
-            . It{"'"}s a Next.js project, so you can deploy it however you like.
-            Check out the README for the recommended solution, using Vercel and
-            Railway.
-          </p>
-          <p className="mt-8 max-w-lg text-sm text-muted-foreground">
-            Using the hosted version? (no longer available){" "}
-            <a
-              href="https://billing.stripe.com/p/login/6oEbJccQOh2Rdji4gg"
-              className="underline"
-            >
-              Go to the billing portal
-            </a>
-            .
+            . É um projeto Next.js, 
+            então você pode fazer o deploy da maneira que preferir. 
+            Confira o README para a solução recomendada, usando Vercel e Railway.
           </p>
         </Stage>
       </div>
